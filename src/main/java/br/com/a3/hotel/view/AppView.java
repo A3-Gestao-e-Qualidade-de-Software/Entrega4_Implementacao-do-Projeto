9267cc5bd1ce9c9fba4fbc3191fa90da924c2e45
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-12-08 15:55:12 -0300

    Update AppView.java

--- src/main/java/br/com/a3/hotel/view/AppView.java
+++ src/main/java/br/com/a3/hotel/view/AppView.java
@@ -5,6 +5,12 @@ import br.com.a3.hotel.controller.*;
 import javax.swing.*;
 import java.sql.SQLException;
 
+    /**
+     * Exibe o menu inicial para o usuário.
+     *
+     * @return A opção escolhida pelo usuário.
+     */
+
 public class AppView {
     public static int menuInicial(){
         int i = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opcão: \n" +
@@ -14,6 +20,14 @@ public class AppView {
         return i;
     }
 
+    /**
+     * Exibe o menu para um usuário logado.
+     *
+     * @return A opção escolhida pelo usuário.
+     * @throws SQLException            Exceção relacionada ao acesso ao banco de dados.
+     * @throws ClassNotFoundException Exceção para a classe não encontrada.
+     */
+    
     public static int menuLogado() throws SQLException, ClassNotFoundException {
         try{
 
