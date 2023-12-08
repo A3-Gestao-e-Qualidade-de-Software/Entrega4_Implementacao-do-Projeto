579c5a5b26d88f613a0c576213294b573d6f9a47
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-12-08 15:39:49 -0300

    Update UsuarioDAO.java

--- src/main/java/br/com/a3/hotel/DAO/UsuarioDAO.java
+++ src/main/java/br/com/a3/hotel/DAO/UsuarioDAO.java
@@ -9,13 +9,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Classe que gerencia operações relacionadas aos usuários no banco de dados.
+ */
+
 public class UsuarioDAO {
     private Connection conn;
+
+     /**
+     * Construtor da classe UsuarioDAO.
+     * @param conn conexão com o banco de dados.
+     */
 
     public UsuarioDAO() {
         this.conn = conn;
     }
 
+    /**
+     * Cadastra um novo usuário no banco de dados.
+     *
+     * @param objUsuarioModel Objeto UsuarioModel contendo os dados do usuário a ser cadastrado.
+     * @return true se o cadastro for realizado com sucesso, false caso contrário.
+     * @throws SQLException             se ocorrer um erro durante a execução da operação no banco de dados.
+     * @throws ClassNotFoundException se a classe do driver JDBC não for encontrada.
+     */
+    
     public boolean cadastrarUsuario(UsuarioModel objUsuarioModel) throws SQLException, ClassNotFoundException {
         new ConexaoDAO();
         conn = ConexaoDAO.conectaBD();
@@ -40,6 +58,14 @@ public class UsuarioDAO {
         }
     }
 
+        /**
+     * Autentica um usuário no sistema.
+     *
+     * @param objUsuarioModel Objeto UsuarioModel contendo os dados do usuário a ser autenticado.
+     * @return um ResultSet com os dados do usuário se a autenticação for bem-sucedida, caso contrário retorna null.
+     * @throws SQLException             se ocorrer um erro durante a execução da operação no banco de dados.
+     * @throws ClassNotFoundException se a classe do driver JDBC não for encontrada.
+     */
 
     public ResultSet autenticarUsuario(UsuarioModel objUsuarioModel) throws SQLException, ClassNotFoundException {
         new ConexaoDAO();
